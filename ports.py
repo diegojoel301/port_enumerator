18f60b679bf566b6059212ab7531f4aac9c32e7d
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-01-06 06:08:53 +0100

    Update ports.py

--- ports.py
+++ ports.py
@@ -2,8 +2,15 @@
 # Para ejecutar este script y obtener los puertos: python3 ports.py 2>&1 | grep "[+]" | awk '{print $8}' | tr -d ':'
 from pwn import *
 import socket
+import sys
 
-server = input("[+] Introduce la maquina victima a enumerar: ")
+try:
+    server = sys.argv[1]    
+except:
+    print("Error no especificaste la direccion IP")
+    print("[+] Forma correcta de ejecutar el script:")
+    print("\tpython3 ports.py <ip> 2>&1 | grep \"[+]\" | awk '{print $8}' | tr -d ':'")
+    sys.exit(1)
 
 for port in range(1, 65535):
     try:
